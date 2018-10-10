430c2f5b916efa2e047f39a897c2e8274e3818df
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-10-10 11:31:46 -0700

    config update

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ import {DateTime} from 'luxon'
 export default {
   launchDate: DateTime.fromObject({
     year: 2018,
-    month: 10,
+    month: 11,
     day: 1,
     hour: 0,
     zone: 'America/New_York'
