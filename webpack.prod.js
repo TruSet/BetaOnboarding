bd7755de8268795e5e09b0e577c4617c2e5382b5
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-10-29 11:27:49 -0400

    removes sourcemap

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,6 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map',
     plugins: [
         new UglifyJSPlugin({ sourceMap: true }),
     ],
